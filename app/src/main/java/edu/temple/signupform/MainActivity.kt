739e6fbf1897ccacd5ae3c812aed0623f7d1c260
Rachel <tul52326@temple.edu>
2021-09-16 13:18:44 -0400

    Added constraints and text

--- app/src/main/java/edu/temple/signupform/MainActivity.kt
+++ app/src/main/java/edu/temple/signupform/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.formActivity)
+        setContentView(R.layout.form_activity)
     }
 }
